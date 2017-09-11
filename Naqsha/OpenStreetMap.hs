4bc0c6a59703e1c4142d0c8aae2dd96fabed20a9
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-09-11 11:31:41 +0530

    top level module for open streetmap.

---
+++ Naqsha/OpenStreetMap.hs
@@ -0,0 +1,10 @@
+-- | This is the top level module that needs to be imported to work
+-- with open street map. We expose the basic elements, tags and meta
+-- information from here. We also expose a streaming interface for
+-- elements form this module.
+--
+module Naqsha.OpenStreetMap
+       (
+       ) where
+
+{-# ANN module "HLint: ignore Use import/export shortcut" #-}

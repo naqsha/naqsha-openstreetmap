ab213f2bb442036d35378e10245ce859a9788308
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-09-11 19:44:33 +0530

    minimal OSM api defined.

--- Naqsha/OpenStreetMap/Api.hs
+++ Naqsha/OpenStreetMap/Api.hs
@@ -1,7 +1,15 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE TypeOperators         #-}
 
 module Naqsha.OpenStreetMap.Api
-  ( -- * Api Information
-    module Naqsha.OpenStreetMap.Api.Info
+  ( -- * The OpenStreetMap API
+    OSMApi
+  , module Naqsha.OpenStreetMap.Api.Info
+  , module Naqsha.OpenStreetMap.Api.ContentTypes
   ) where
 
 import Naqsha.OpenStreetMap.Api.Info
+import Naqsha.OpenStreetMap.Api.ContentTypes
+import Servant.API
+
+type OSMApi  = "api" :> "capabilities" :> Get '[XML, XMLPretty] Api
